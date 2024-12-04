baed56d13aff3c5d42b1c435fb813de93dc3ae49
Teun van Brakel <teunvbrakel@gmail.com>
2024-12-04 20:30:38 +0100

    solve 1

--- day3/aoc-day3.hs
+++ day3/aoc-day3.hs
@@ -6,9 +6,11 @@ solve :: [Char]-> Integer
 --solve :: [Char] -> [Int]
 solve [] = 0
 solve (_:[]) = 0
+solve (a:b:[]) = 0
+solve (a:b:c:[]) = 0
 solve (a:b:c:d:[]) = 0
 solve (a:b:c:d:xs) = if [a, b, c, d] == "mul("
-    then fromIntegral (splitString (take (fromInteger (getIndexOfClosing xs ')')) xs)) + solve (drop (fromInteger (getIndexOfClosing xs ')')) xs)
+    then fromIntegral (splitString (take (fromInteger (getIndexOfClosing xs ')')) xs)) + solve xs
     else solve (b:c:d:xs)
 
 getIndexOfClosing :: [Char] -> Char -> Integer
@@ -21,12 +23,11 @@ splitString x =
         secondSubstring = drop (fromIntegral closingIndex + 1) x
         toNumber y = foldl (\acc l -> acc * 10 + l) 0 (map ((\l -> l - 48) . fromEnum) y)
         checking y = foldr (\l acc -> l >= 10 || acc) False (map ((\l -> l - 48) . fromEnum) y)
-    in  if checking substring || checking secondSubstring then 0 else toNumber substring * toNumber secondSubstring
+        fullLength y = foldr(\l acc -> 1 + acc) 0 (map ((\l -> l - 48) . fromEnum) y)
+    in  if fullLength secondSubstring > 3 || fullLength substring > 3 || checking substring || checking secondSubstring then 0 else (toNumber substring * toNumber secondSubstring)
 
 main :: IO ()
 main = do
   input <- readFile "input.txt"
-  --let parseLine = map read . words
-  --let rows = (map parseLine . lines) input
-  print (splitString "123,456")
   print (solve input)
+--167650499 correct
