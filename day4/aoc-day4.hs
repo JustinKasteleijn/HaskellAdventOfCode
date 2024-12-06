a00c356a09ee7599b0620bd032c552ca18daa350
Justin Kasteleijn <justin.kasteleijn@gmail.com>
2024-12-06 14:57:36 +0100

    I hate day 4

--- day4/aoc-day4.hs
+++ day4/aoc-day4.hs
@@ -1,23 +1,57 @@
 import Data.List
- 
-getElement :: [[Char]] -> Int -> Int -> Maybe Char
-getElement grid row col =
-    if row < length grid
-    then case (grid !! row) of
-        r -> if col < length r then Just (r !! col) else Nothing
-    else Nothing
+import Data.Maybe
 
-     
-countHorizontal :: [Char] -> Integer
-countHorizontal xs = foldr countMatches 0 (tails xs)
-  where
-    countMatches sublist acc
-      | "XMAS" `isPrefixOf` sublist = acc + 1
-      | "SAMX" `isPrefixOf` sublist = acc + 1
-      | otherwise = acc
+occurCount :: (Eq a) => [a] -> [a] -> Int
+occurCount word = 
+  length .
+  filter (flip elem [word, (reverse word)]) .
+  windows (length word)
 
+--Use nothing as padding and transpose the matrix to make the diagonals columns
+diagonals :: [[a]] -> [[a]]
+diagonals =
+  map catMaybes . 
+  transpose . 
+  zipWith (++) (inits (repeat Nothing)) . 
+  (map . map) Just
+    
+windows :: Int -> [a] -> [[a]]
+windows n xs = foldr window [] (tails xs)
+   where 
+     window l acc 
+       | length l >= n = take n l : acc
+       | otherwise    = acc
+
+solve1 :: [[Char]] -> Int
+solve1 xs =
+    sum $
+    map
+      (occurCount "XMAS")
+      ( xs                     
+          ++ transpose xs
+          ++ diagonals xs
+          ++ diagonals (reverse xs)
+      )
+      
+windows2d :: Int -> [[a]] -> [[[a]]]
+windows2d n = concatMap (transpose . map (windows n)) . windows n
+      
+isXMAS3x3 :: [[Char]] -> Bool
+isXMAS3x3
+    [ [ul, _, ur],
+      [_, 'A', _],
+      [ll, _, lr]
+    ] = all (`elem` ["MS", "SM"]) [[ul, lr], [ur, ll]]
+isXMAS3x3 _ = False
+   
+solve2 :: [[Char]] -> Int 
+solve2 = 
+   length . 
+   filter isXMAS3x3 .
+   windows2d 3
+       
 main :: IO ()
 main = do
-  input <- readFile "input_test.txt"
+  input <- readFile "input.txt"
   let parse = words
-  print $ countHorizontal "SAMXXXMASM"
+  print $ solve2 $ parse input
